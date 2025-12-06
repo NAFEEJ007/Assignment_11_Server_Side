854ff173b867078271ca55dc329ac2abc2deff5f
Nafeej <nafeejtamjeed@gmail.com>
2025-12-06 16:06:05 +0600

    Add TLS options to MongoDB client to fix SSL handshake error

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ const client = new MongoClient(uri, {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
+    tls: true,
+    tlsAllowInvalidCertificates: true, 
 });
 
 // Verify Token Middleware
